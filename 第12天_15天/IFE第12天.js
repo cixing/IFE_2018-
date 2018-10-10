84d7496f78644255f4d89933c313304641546c9e
cixing <740174648@qq.com>
2018-10-10 17:10:15 +0800

    继续添加

---
+++ 第12天_15天/IFE第12天.js
@@ -0,0 +1,21 @@
+/*
+* @Author: Cixing
+* @Date:   2018-09-27 11:07:43
+* @Last Modified by:   Cixing
+* @Last Modified time: 2018-09-27 11:07:57
+* @Email: 740174648@qq.com
+*/
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">    
+    <title>IFE ECMAScript</title>
+</head>
+<body>
+    <h1>Demo</h1>
+    <p id="content-wrapper">Hello World</p>
+    <script>
+        console.log(document.getElementById("content-wrapper").innerHTML);
+    </script>
+</body>
+</html>
